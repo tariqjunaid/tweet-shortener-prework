692d5c03b2ffe081f4ca13b3e8d226ce8912fcf9
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-03-02 19:22:33 +0000

    Done.

--- spec/tweet_shortener_spec.rb
+++ spec/tweet_shortener_spec.rb
@@ -19,7 +19,7 @@ describe 'tweet shortener' do
   # Question 1
   describe '#word_substituter' do
     it 'replaces long words with their expected short form' do
-      expect(word_substituter(tweet_one)).to eq(tweet_one_short)
+      expect(word_substituter(tweet_five)).to eq(tweet_five_short)
     end
   end
 
--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def dictionary
+  subs = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split.map do |word|
+    dictionary.keys.include?(word.downcase) ? word = dictionary[word.downcase] : word
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.map { |word|  puts word_substituter(word) }
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet.length > 140 ? tweet[0..136] << "..." : tweet
+end
